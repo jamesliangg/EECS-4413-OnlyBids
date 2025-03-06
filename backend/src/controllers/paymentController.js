582ea485b4188b5ff480e94888d28c3a39be38d3
mosesluna <mosesluna905@gmail.com>
2025-03-06 18:00:22 -0500

    added and partially implemented paymentModel and paymentController

---
+++ backend/src/controllers/paymentController.js
@@ -0,0 +1,60 @@
+const paymentModel = require("../models/paymentModel");
+
+const paymentController = {
+  attemptPayment: async (req, res) => {
+    try {
+      // Get the request body
+      const {
+        auction_id,
+        buyer_id,
+        winning_id,
+        cardNum,
+        cardholder,
+        expDate,
+        cvv,
+      } = req.body;
+
+      // Validate auction
+
+      // Validate input
+      if (!cardNum || !cardholder || !expDate || !cvv) {
+        return res.status(400).json({ error: "All fields are required" });
+      }
+
+      // Validate card number
+      if (cardNum != new RegExp("^[0-9]{16}$")) {
+        return res.status(400).json({ error: "Invalid card number" });
+      }
+
+      // Validate cardholder name
+      if (cardholder != new RegExp("^[a-zA-Z]{,26}s{,1}[a-zA-Z]{,26}$")) {
+        return res.status(400).json({ error: "Invalid cardholder name" });
+      }
+
+      // Validate expiry date
+      const date = new Date();
+      let month = date.getMonth();
+      let year = date.getFullYear();
+      if (
+        expDate != new RegExp("^[0-12]{2}/{1}[0-99]{2}$") &&
+        (expDate.substring(3, 5) < year.substring(2, 4) ||
+          (expDate.substring(3, 5) > year.substring(2, 4) &&
+            expDate.substring(3, 5) < month))
+      ) {
+        return res.status(400).json({ error: "Invalid card expiry date" });
+      }
+
+      // Validate cvv number
+      if (cvv != new RegExp("^[0-9]{3}$")) {
+        return res.status(400).json({ error: "Invalid CVV number" });
+      }
+
+      const payment = await paymentModel.createPayment({});
+    } catch (error) {
+      console.error("Payment error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  },
+};
+
+module.exports = paymentController;
